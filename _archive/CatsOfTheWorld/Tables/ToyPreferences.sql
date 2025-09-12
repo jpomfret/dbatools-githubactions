7857b50fb7aa06e50d94967052e2491e3738d777
jpomfret <jpomfret7@gmail.com>
2025-09-12 13:17:50 +0100

    remove '

--- _archive/CatsOfTheWorld/Tables/ToyPreferences.sql
+++
@@ -1,8 +0,0 @@
--- Table: ToyPreferences (because every cat is unique)
-CREATE TABLE dbo.ToyPreferences (
-    CatId INT FOREIGN KEY REFERENCES dbo.Cats(CatId),
-    ToyId INT FOREIGN KEY REFERENCES dbo.Toys(ToyId),
-    PreferenceLevel INT CHECK (PreferenceLevel BETWEEN 1 AND 5),
-    PRIMARY KEY (CatId, ToyId)
-);
-GO
