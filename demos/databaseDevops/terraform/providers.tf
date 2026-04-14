220880990b3ac5327e0c9c6a87ea2cf7a5b96428
jpomfret <jpomfret7@gmail.com>
2026-04-14 13:35:05 +0100

    add: update .gitignore and configure backend for Terraform state management

--- demos/databaseDevops/terraform/providers.tf
+++ demos/databaseDevops/terraform/providers.tf
@@ -11,6 +11,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stcotwterraformstate"
+    container_name       = "tfstate"
+    key                  = "cotw.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
